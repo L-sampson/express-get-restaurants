8d542aad64d15b2925a8734e045e12bb99ef080b
L-sampson <lavonsampson@google.com>
2023-10-12 23:42:55 -0400

    Updated app.js file. Created route path to /restaurants.Found all of the restaurants and returned the response as a JSON res. Output on local server was correct.

--- src/app.js
+++ src/app.js
@@ -4,7 +4,15 @@ const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
-
+app.get('/restaurants', async (req, res)=>{
+    try{
+        const restaurants = await Restaurant.findAll();
+        res.json(restaurants);
+    }
+    catch(error){
+        res.status(500).json({error:'An error occured when retriving restaurants'})
+    }
+})
 
 
 
